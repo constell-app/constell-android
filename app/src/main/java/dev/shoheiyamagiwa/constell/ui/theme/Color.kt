8101f6606b3ca5600b68de31a221580445868105
shohei-yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2026-03-02 02:25:39 +0900

    refactor: organize color definition order

--- app/src/main/java/dev/shoheiyamagiwa/constell/ui/theme/Color.kt
+++ app/src/main/java/dev/shoheiyamagiwa/constell/ui/theme/Color.kt
@@ -3,22 +3,22 @@ package dev.shoheiyamagiwa.constell.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
+val Purple500 = Color(0xFFA855F7)
+
 val PurpleGrey40 = Color(0xFF625b71)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+
+val Pink80 = Color(0xFFEFB8C8)
 val Pink40 = Color(0xFF7D5260)
 
-val Slate900 = Color(0xFF020618)
-val Slate800 = Color(0xFF1E293B)
-val Slate600 = Color(0xFF475569)
 val Slate300 = Color(0xFFCBD5E1)
+val Slate600 = Color(0xFF475569)
+val Slate800 = Color(0xFF1E293B)
+val Slate900 = Color(0xFF020618)
 
 val Blue400 = Color(0xFF60A5FA)
 val Blue500 = Color(0xFF3B82F6)
 
 val Sky800 = Color(0xFF024a70)
 val Sky900 = Color(0xFF052f4a)
-
-val Purple500 = Color(0xFFA855F7)
