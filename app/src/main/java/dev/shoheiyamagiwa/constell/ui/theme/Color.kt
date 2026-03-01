fab88c7f7ad1dc3780e9d5b3dd8ff0a5524d02a9
shohei-yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2026-03-01 23:05:40 +0900

    feat: add some colors

--- app/src/main/java/dev/shoheiyamagiwa/constell/ui/theme/Color.kt
+++ app/src/main/java/dev/shoheiyamagiwa/constell/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Slate800 = Color(0xFF1E293B)
+val Slate600 = Color(0xFF475569)
+
+val Slate300 = Color(0xFFCBD5E1)
+val Blue400 = Color(0xFF60A5FA)
+val Blue500 = Color(0xFF3B82F6)
+
+val Purple500 = Color(0xFFA855F7)
