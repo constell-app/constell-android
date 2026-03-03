73c32fcc7a266aa503ccc1f43f29369cc37a793f
shohei-yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2026-03-03 23:25:15 +0900

    feat: add new color definitions

--- app/src/main/java/dev/shoheiyamagiwa/constell/ui/theme/Color.kt
+++ app/src/main/java/dev/shoheiyamagiwa/constell/ui/theme/Color.kt
@@ -3,22 +3,28 @@ package dev.shoheiyamagiwa.constell.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
-val Purple40 = Color(0xFF6650a4)
+val Purple40 = Color(0xFF6650A4)
 val Purple500 = Color(0xFFA855F7)
+val Purple600 = Color(0xFF9333EA)
 
-val PurpleGrey40 = Color(0xFF625b71)
+val PurpleGrey40 = Color(0xFF625B71)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 
 val Pink80 = Color(0xFFEFB8C8)
 val Pink40 = Color(0xFF7D5260)
 
+val Slate100 = Color(0xFFF1F5F9)
 val Slate300 = Color(0xFFCBD5E1)
+val Slate400 = Color(0xFF94A3B8)
 val Slate600 = Color(0xFF475569)
+val Slate700 = Color(0xFF334155)
 val Slate800 = Color(0xFF1E293B)
-val Slate900 = Color(0xFF020618)
+val Slate900 = Color(0xFF0F172A)
+val Slate950 = Color(0xFF020618)
 
 val Blue400 = Color(0xFF60A5FA)
 val Blue500 = Color(0xFF3B82F6)
+val Blue600 = Color(0xFF2563EB)
 
-val Sky800 = Color(0xFF024a70)
-val Sky900 = Color(0xFF052f4a)
+val Sky800 = Color(0xFF024A70)
+val Sky900 = Color(0xFF052F4A)
