9100ab6852e257bd42c62f281bf8d61aa324c76b
shohei-yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2026-03-29 20:31:15 +0900

    fix: center the loading indicator

--- app/src/main/java/dev/shoheiyamagiwa/constell/feature/home/HomeScreen.kt
+++ app/src/main/java/dev/shoheiyamagiwa/constell/feature/home/HomeScreen.kt
@@ -1,10 +1,13 @@
 package dev.shoheiyamagiwa.constell.feature.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -25,7 +28,9 @@ public fun HomeScreen(viewModel: HomeViewModel = koinViewModel()) {
     Background(modifier = Modifier.dotBackground(dotColor = Slate600, dotRadius = 1.5.dp, spacing = 30.dp, alpha = 0.3F)) {
         when (val currentState = screenState) {
             is HomeScreenState.Loading -> {
-                CircularProgressIndicator()
+                Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                    CircularProgressIndicator()
+                }
             }
 
             is HomeScreenState.Default -> {
