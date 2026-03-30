71b506c194f985b0fdcc0b20ba3f4d14fa1393f7
shohei-yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2026-03-31 01:33:52 +0900

    Reapply "test: add unit tests for ViewModel validation"
    This reverts commit 7787a9f2cc89f685f21f87db8291fb05ff9ad3bd.

---
+++ app/src/test/java/dev/shoheiyamagiwa/constell/MainViewModelTest.kt
@@ -0,0 +1,53 @@
+package dev.shoheiyamagiwa.constell
+
+import dev.shoheiyamagiwa.constell.data.repository.UserPreferencesRepository
+import dev.shoheiyamagiwa.constell.util.MainDispatcherRule
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+public class MainViewModelTest {
+    @get:Rule
+    public val mainDispatcherRule = MainDispatcherRule()
+
+    private val repository: UserPreferencesRepository = mockk()
+
+    @Test
+    public fun `initial state becomes Success when repository emits values`() = runTest {
+        val isLoggedInFlow = MutableStateFlow(value = false)
+        val isFirstLaunchFlow = MutableStateFlow(value = true)
+        every { repository.isLoggedIn } returns isLoggedInFlow
+        every { repository.isFirstLaunch } returns isFirstLaunchFlow
+
+        val viewModel = MainViewModel(repository)
+        runCurrent()
+        assertEquals(MainUiState.Success(isLoggedIn = false, isFirstLaunch = true), viewModel.uiState.value)
+    }
+
+    @Test
+    public fun `uiState updates when repository flows emit new values`() = runTest {
+        val isLoggedInFlow = MutableStateFlow(value = false)
+        val isFirstLaunchFlow = MutableStateFlow(value = true)
+        every { repository.isLoggedIn } returns isLoggedInFlow
+        every { repository.isFirstLaunch } returns isFirstLaunchFlow
+
+        val viewModel = MainViewModel(repository)
+        runCurrent()
+        assertEquals(MainUiState.Success(isLoggedIn = false, isFirstLaunch = true), viewModel.uiState.value)
+
+        isFirstLaunchFlow.value = false
+        runCurrent()
+        assertEquals(MainUiState.Success(isLoggedIn = false, isFirstLaunch = false), viewModel.uiState.value)
+
+        isLoggedInFlow.value = true
+        runCurrent()
+        assertEquals(MainUiState.Success(isLoggedIn = true, isFirstLaunch = false), viewModel.uiState.value)
+    }
+}
